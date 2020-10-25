c4cea072ec50ca89fc295f0a8a12ac904eb26576
Ifan Fairuz <ifanfairuz@gmail.com>
2020-10-25 11:35:07 +0700

    [ADD] implement router and google login

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import { LoaderPlugin } from 'vue-google-login'
 
 Vue.config.productionTip = false
+Vue.use(LoaderPlugin, {
+  client_id: '852266590854-187280ijdp2cer8pgmh6gkemlnl4osnv.apps.googleusercontent.com'
+})
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
