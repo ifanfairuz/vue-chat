b29b5201c84e43c07772d6715a5d02af2ef468a3
Ifan Fairuz <ifanfairuz@gmail.com>
2020-10-25 11:36:11 +0700

    [ADD] login

---
+++ src/handler/login/index.js
@@ -0,0 +1,14 @@
+import session, { generateProfile } from './session'
+import logout from './logout'
+
+export {
+  generateProfile,
+  session,
+  logout
+}
+
+export default {
+  generateProfile,
+  session,
+  logout
+}
